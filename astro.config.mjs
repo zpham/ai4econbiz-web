9fa4c0bf8842c1f445624e2a76610c3bf9add8b7
kimzung <zungptk@epu.edu.vn>
2026-05-29 18:16:00 +0700

    Update first time

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
-import tailwindcss from '@tailwindcss/vite';
-
-// https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  integrations: [tailwind()],
 });
